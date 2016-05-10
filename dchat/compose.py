e0712e7ba2a77c22f53b4a59ea9a66e070965c52
lq0000 <root@lo.calho.st>
2016-05-10 16:02:13 -0600

    add TwoWayComposer and TwoWayExtendingComposer

--- dchat/compose.py
+++ dchat/compose.py
@@ -1,3 +1,4 @@
+import collections
 from dchat.storage import EmptyResponseError
 
 class SimpleComposer (object):
@@ -13,4 +14,76 @@ class SimpleComposer (object):
                 phrase.append(seq[-1])
             except EmptyResponseError:
                 stop = True
+        return " ".join(phrase)
+
+class TwoWayComposer (object):
+
+    def __init__(self, backend):
+        self._st = backend
+    
+    def generate(self):
+        seed, start, stop = self._st.getRow()
+        phrase = collections.deque(seed)
+        
+        while not start:
+            try:
+                seq, start, _ = self._st.getRowEndingWith(phrase[0], phrase[1])
+                phrase.appendleft(seq[0])
+            except EmptyResponseError:
+                start = True
+        
+        while not stop:
+            try:
+                seq, _, stop = self._st.getRowStartingWith(phrase[-2], phrase[-1])
+                phrase.append(seq[-1])
+            except EmptyResponseError:
+                stop = True
+                
+        return " ".join(phrase)
+
+class TwoWayExtendingComposer (object):
+
+    def __init__(self, backend, targetLength = 4, maxAttempts = 10):
+        self._st = backend
+        self._targetLength = targetLength
+        self._maxAttempts = maxAttempts
+    
+    def generate(self):
+        seed, start, stop = self._st.getRow()
+        phrase = collections.deque(seed)
+
+        attempts = 0
+        count = 0
+        
+        while not (start and (count >= self._targetLength or attempts >= self._maxAttempts)):
+            try:
+                seq, start, _ = self._st.getRowEndingWith(phrase[0], phrase[1])
+                phrase.appendleft(seq[0])
+                count += 1
+            except EmptyResponseError:
+                phrase.popleft()
+                count -=1
+                if count > 0:
+                    phrase.popleft()
+                    count -= 1
+                attempts += 1
+                start = False
+        
+        attempts = 0
+        count = 0
+        
+        while not (stop and (count >= self._targetLength or attempts >= self._maxAttempts)):
+            try:
+                seq, _, stop = self._st.getRowStartingWith(phrase[-2], phrase[-1])
+                phrase.append(seq[-1])
+                count += 1
+            except EmptyResponseError:
+                phrase.pop()
+                count -= 1
+                if count > 0:
+                    phrase.pop()
+                    count -= 1
+                attempts += 1
+                stop = False
+                
         return " ".join(phrase)
--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import dchat
 
 def main():
     storage = dchat.storage.SqliteBackend("brain.db")
-    composer = dchat.compose.SimpleComposer(storage)
+    composer = dchat.compose.TwoWayExtendingComposer(storage)
     learner = dchat.learn.SimpleLearner(storage)
     
     phrases = [
         "hello world this is denice",
         "this is not the time for shenanigans",
-        "the time for action is now"
+        "the time for action is now",
+        "is denice here right now?",
+        "i like toast with peanut butter",
+        "i like cake and ice cream",
+        "peanut butter jelly time"
     ]
     
     for p in phrases:
