db154910f8df025cba6b9aa5d00295ad8e5bb860
lq0000 <root@lo.calho.st>
2016-05-16 13:37:07 -0600

    modify graph composer to be less expoential in runtime and memory consumption

--- dchat/compose.py
+++ dchat/compose.py
@@ -1,6 +1,5 @@
-import collections, itertools, random
+import collections, itertools, random, math
 from dchat.storage import EmptyResponseError
-from dchat.utils import WordTree
 
 class SimpleComposer (object):
 
@@ -46,8 +45,8 @@ class TwoWayExtendingComposer (object):
 
     def __init__(self, backend, targetLength = 8, maxAttempts = 10):
         self._st = backend
-        self._targetLength = targetLength
-        self._maxAttempts = maxAttempts
+        self._targetLength = int(targetLength)
+        self._maxAttempts = int(maxAttempts)
     
     def generate(self):
         seed, start, stop = self._st.getRow()
@@ -91,101 +90,87 @@ class TwoWayExtendingComposer (object):
 
 class SeededTreeComposer (object):
 
-    def __init__(self, backend, seed = None, targetLength = 8, iters = None):
+    def __init__(self, backend, seed = None, targetLength = 4, breadth = 8, depth = 2):
         self._st = backend
         self.seed = seed
-        self.target = targetLength
-        self.iters = iters
-        if iters == None:
-            self.iters = 2 * targetLength
+        self.target  = int(targetLength)
+        self.depth   = int(depth)
+        self.breadth = int(breadth)
     
     def generate(self):
     
-        seed, start, stop = None, False, False
-        
+        seed = None
+    
         if self.seed != None:
             try:
-                seed, start, stop = self._st.getRowContaining(self.seed)
+                seed, _, _ = self._st.getRowContaining(self.seed)
             except EmptyResponseError:
                 pass
         
         if seed == None:
-            seed, start, stop = self._st.getRow()
+            seed, _, _ = self._st.getRow()
         
-        t = WordTree()
-        t.insert(*itertools.chain(seed, [start, stop]))
-        
-        for i in xrange(0, self.iters):
-            for n in t.nodes():
-                if not n.start:
-                    try:
-                        seq, start, stop = self._st.getRowEndingWith(n.tup[0], n.tup[1])
-                        if not tuple(seq) in set(tuple(x.tup) for x in n.preds):
-                            m = t.insert(*itertools.chain(seq, [start, stop]))
-                            n.preds.append(m)
-                            m.parent = n
-                    except EmptyResponseError:
-                        n.start = True
-                if not n.end:
-                    try:
-                        seq, start, stop = self._st.getRowStartingWith(n.tup[1], n.tup[2])
-                        if not tuple(seq) in set(tuple(x.tup) for x in n.succs):
-                            m = t.insert(*itertools.chain(seq, [start, stop]))
-                            n.succs.append(m)
-                            m.parent = n
-                    except EmptyResponseError:
-                        n.end = True
-                        
-        starts = t.starts()
-        ends = set(t.ends())
-        
-        def successor_paths(n):
-            for s in n.succs:
-                if s in ends:
-                    yield [s]
-                else:
-                    for c in successor_paths(s):
-                        p2 = [s]
-                        p2.extend(c)
-                        yield p2
-        
-        def compress(path):
-            res = list(path[0].tup)
-            for n in path[1:]:
-                res.append(n.tup[2])
-            return res
-        
-        def get_weight(phrase):
-            return 1. / ((abs(self.target - len(x)) + 1))
-        
+        leaves = set()
         candidates = set()
+    
+        # extend left side of tree
+        nodes, next = [ ], [seed]
+        for i in xrange(self.depth):
+            nodes, next = next, nodes
+            for prev in nodes:
+                for j in xrange(0, self.breadth):
+                    try:
+                        seq, _, _ = self._st.getRowEndingWith(prev[0], prev[1])
+                        next.append(seq)
+                    except EmptyResponseError:
+                        pass
+        leaves |= set(tuple(x) for x in next)
         
-        for s in starts:
+        # extend right side of tree
+        nodes, next = [ ], [seed]
+        for i in xrange(self.depth):
+            nodes, next = next, nodes
+            for prev in nodes:
+                for j in xrange(0, self.breadth):
+                    try:
+                        seq, _, _ = self._st.getRowStartingWith(prev[1], prev[2])
+                        next.append(seq)
+                    except EmptyResponseError:
+                        pass
+        leaves |= set(tuple(x) for x in next)
         
-            parents = []
-            p = s
-            while p != None and (s is p or s in p.preds):
-                parents.append(p)
-                p = p.parent
+        # extend each leaf node into a phrase
+        for seed in leaves:
+            phrase = collections.deque(seed)
+            start, stop = False, False
             
-            for i in xrange(0, len(parents)):
-                prefix = parents[:i+1]
-                root = parents[i]
-                succs = [x for x in successor_paths(root)]
-                for succ in succs:
-                    candidates.add(tuple(itertools.chain(prefix, succ)))
-
-        compressed = set()
-
-        for c in candidates:
-            compressed.add(tuple(compress(c)))
+            while not start:
+                try:
+                    seq, start, _ = self._st.getRowEndingWith(phrase[0], phrase[1])
+                    phrase.appendleft(seq[0])
+                except EmptyResponseError:
+                    start = True
             
-        compressed = sorted(compressed, key=get_weight, reverse=True)
-        total = sum(get_weight(x) for x in compressed)
+            while not stop:
+                try:
+                    seq, _, stop = self._st.getRowStartingWith(phrase[-2], phrase[-1])
+                    phrase.append(seq[-1])
+                except EmptyResponseError:
+                    stop = True
+            
+            candidates.add(tuple(phrase))
+        
+        #  weighted selection based on deviation from target length
+        def get_weight(phrase):
+            return 1. / ((abs(self.target - len(phrase)) + 1))
+        
+        candidates = sorted(candidates, key=get_weight, reverse=True)
+        total = sum(get_weight(x) for x in candidates)
         r = random.uniform(0, total)
         acc = 0
-        for c in compressed:
+        for c in candidates:
             acc += get_weight(c)
             if acc > r:
                 return " ".join(c)
+        
             
